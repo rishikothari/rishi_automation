3371fb25c007dba36b18481389cb9b06c14743a3
rishikesh <rishikeshkothari@gmail.com>
2015-02-09 14:36:22 +0530

    added

--- src/testHarness/ImdbTop50.java
+++ src/testHarness/ImdbTop50.java
@@ -11,10 +11,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
- 
-
-
-
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -29,145 +25,160 @@ import ObjectRepository.ObjectRepository;
 public class ImdbTop50 extends Setup {
 
 	WebDriver driver = new FirefoxDriver();
-	
-//		 WebDriver driver = Setup.driver;
-	 
-	 public void waitForPageToLoad(){
-		 driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
-	 }
-	 public void navigateToHomePage(){
-	
-		 driver.get("http://www.imdb.com");
-		 waitForPageToLoad();
-	 }
-	 
-	 public void clickOnWatchlist(){
-		 driver.findElement(By.xpath((ObjectRepository.ImdbElements.watchList))).click();
-		 waitForPageToLoad();	 
-	 }
-	 
-	 public void clickOnTop250Link(){
-		 driver.findElement(By.xpath((ObjectRepository.ImdbElements.top250Link))).click();		
-//		 waitForPageToLoad();	 
-	 }
-	 public void getMovieTitle(){
-		 //driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
-		 for (int i=1; i<=4 ; i++)
-		 {
-		 System.out.println(driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle.replace("rowNum", Integer.toString(i)))).getText());
-		 }
-	 }
 
-	 public void getReleaseYear(){
-		 //driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
-		 for (int i=1; i<=4 ; i++)
-		 {
-		 System.out.println(driver.findElement(By.xpath(ObjectRepository.ImdbElements.releaseYear.replace("rowNum", Integer.toString(i)))).getText());
-		 }
-	 }
-	 
-	 public void getMovieRating(){
-		 //driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
-		 for (int i=1; i<=4 ; i++)
-		 {
-		 System.out.println(driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieRating.replace("rowNum", Integer.toString(i)))).getText());
-		 }
-	 }
-	 
-	 public void storeValueinList(){
-		HashMap<String, String>  map = new HashMap<String, String>(); 
-		for ( int i=1; i<250; i++){
-		 map.put("name",driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle.replace("rowNum", Integer.toString(i)))).getText());
-		map.put("year",	driver.findElement(By.xpath(ObjectRepository.ImdbElements.releaseYear.replace("rowNum", Integer.toString(i)))).getText());
-		map.put("rating",driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieRating.replace("rowNum", Integer.toString(i)))).getText());
-		databaseInsert(map);
+	// WebDriver driver = Setup.driver;
+
+	public void waitForPageToLoad() {
+		driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
+	}
+
+	public void navigateToHomePage() {
+
+		driver.get("http://www.imdb.com");
+		waitForPageToLoad();
+	}
+
+	public void clickOnWatchlist() {
+		driver.findElement(By.xpath((ObjectRepository.ImdbElements.watchList)))
+				.click();
+		waitForPageToLoad();
+	}
+
+	public void clickOnTop250Link() {
+		driver.findElement(By.xpath((ObjectRepository.ImdbElements.top250Link)))
+				.click();
+		// waitForPageToLoad();
+	}
+
+	public void getMovieTitle() {
+		// driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(driver.findElement(
+					By.xpath(ObjectRepository.ImdbElements.movieTitle.replace(
+							"rowNum", Integer.toString(i)))).getText());
 		}
-		//		System.out.println(map.);
-	 }
+	}
+
+	public void getReleaseYear() {
+		// driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(driver.findElement(
+					By.xpath(ObjectRepository.ImdbElements.releaseYear.replace(
+							"rowNum", Integer.toString(i)))).getText());
+		}
+	}
+
+	public void getMovieRating() {
+		// driver.findElement(By.xpath(ObjectRepository.ImdbElements.movieTitle)).getText();
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(driver.findElement(
+					By.xpath(ObjectRepository.ImdbElements.movieRating.replace(
+							"rowNum", Integer.toString(i)))).getText());
+		}
+	}
+
+	public void storeValueinList() {
+		HashMap<String, String> map = new HashMap<String, String>();
+		for (int i = 1; i < 250; i++) {
+			map.put("name",
+					driver.findElement(
+							By.xpath(ObjectRepository.ImdbElements.movieTitle
+									.replace("rowNum", Integer.toString(i))))
+							.getText());
+			map.put("year",
+					driver.findElement(
+							By.xpath(ObjectRepository.ImdbElements.releaseYear
+									.replace("rowNum", Integer.toString(i))))
+							.getText());
+			map.put("rating",
+					driver.findElement(
+							By.xpath(ObjectRepository.ImdbElements.movieRating
+									.replace("rowNum", Integer.toString(i))))
+							.getText());
+			databaseInsert(map);
+		}
+		// System.out.println(map.);
+	}
+
 	public void printFile() {
 		// TODO Auto-generated method stub
 
 		printFileFromDB();
 
 	}
-		  
-			
-			  public void printFileFromDB(){
-				  
-				 // Map<String, String>  outputMap = new HashMap<String, String>();
 
-				    Connection c = null;
-				    Statement stmt = null;
-				    BufferedWriter bw =null;
-				    FileWriter fw;
-				    
-				    
-					 try{
-						 File file = new File("filename3.txt");
-		 
-						// if file doesnt exists, then create it
-						if (!file.exists()) {
-							file.createNewFile();
-						}
-			 
-						 fw = new FileWriter(file.getAbsoluteFile());
-						 bw = new BufferedWriter(fw);
-					 }
-					 catch(Exception e){
-						 
-					 }
-				  
-//				    String title = outputMap.get("name");
-//				    String release = outputMap.get("year");
-//				    String rating =outputMap.get("rating");
-				    
-				    //static  int count =1;g
-				    try {
-				      Class.forName("org.sqlite.JDBC");
-				      c = DriverManager.getConnection("jdbc:sqlite:test4.db");
-				      //c.setAutoCommit(false);
-				      System.out.println("Opened database successfully "+c.toString());
-				      
-				      stmt = c.createStatement();
-				      
-				      String sql = "Select * from imdbrating5;";  
-				      
-				      ResultSet rs = stmt.executeQuery(sql);
-				      
-				    
+	public void printFileFromDB() {
 
-				      while (rs.next()) {
-		
-				          System.out.println("Name= " + rs.getString(1));
-				          System.out.println("Name2= " + rs.getString(2));
-				          System.out.println("Name3= " + rs.getString(3));
-				          System.out.println("Name3= " + rs.getString(4));
-				         
-//				          outputMap.put("name", rs.getString(2));
-//				          outputMap.put("year",rs.getString(3) );
-//				          outputMap.put("rating",rs.getString(4) );
-				          
-//				          String  outputName = rs.getString(2);
-//							String  outputYear = rs.getString(3);
-//							String  outputRating = rs.getString(4);
-							String fileOutput = rs.getString(1)+"  "+ rs.getString(2) + "  "+ "  "+rs.getString(3) + "  "+ rs.getString(4)+"\n";
-				          if(fileOutput != null){
-				        	  bw.write(fileOutput);
-				  			}
-				         
-				      }
-				      stmt.close();
-				      c.close();
-				      bw.close();
-				      
-				    } catch ( Exception e ) {
-				      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-				      System.exit(0);
-				    }
-				    
-				  
-				  
-				  //return outputMap;
-				  
-			  }
+		// Map<String, String> outputMap = new HashMap<String, String>();
+
+		Connection c = null;
+		Statement stmt = null;
+		BufferedWriter bw = null;
+		FileWriter fw;
+
+		try {
+			File file = new File("filename3.txt");
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			fw = new FileWriter(file.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
+		} catch (Exception e) {
+
+		}
+
+		// String title = outputMap.get("name");
+		// String release = outputMap.get("year");
+		// String rating =outputMap.get("rating");
+
+		// static int count =1;g
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test4.db");
+			// c.setAutoCommit(false);
+			System.out.println("Opened database successfully " + c.toString());
+
+			stmt = c.createStatement();
+
+			String sql = "Select * from imdbrating5;";
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+
+				System.out.println("Name= " + rs.getString(1));
+				System.out.println("Name2= " + rs.getString(2));
+				System.out.println("Name3= " + rs.getString(3));
+				System.out.println("Name3= " + rs.getString(4));
+
+				// outputMap.put("name", rs.getString(2));
+				// outputMap.put("year",rs.getString(3) );
+				// outputMap.put("rating",rs.getString(4) );
+
+				// String outputName = rs.getString(2);
+				// String outputYear = rs.getString(3);
+				// String outputRating = rs.getString(4);
+				String fileOutput = rs.getString(1) + "  " + rs.getString(2)
+						+ "  " + "  " + rs.getString(3) + "  "
+						+ rs.getString(4) + "\n";
+				if (fileOutput != null) {
+					bw.write(fileOutput);
+				}
+
+			}
+			stmt.close();
+			c.close();
+			bw.close();
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		// return outputMap;
+
+	}
 }
